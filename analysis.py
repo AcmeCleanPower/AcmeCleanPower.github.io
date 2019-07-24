fc5216c5f82924695cbaf04d0782ccae611ad169
Elliot <terramars@gmail.com>
2019-07-24 12:24:37 -0700

    heatmap script update

--- analysis.py
+++ analysis.py
@@ -17,15 +17,23 @@ def get_heatmap(data, kernel_area=1000000, resolution=30):
 
 
 def generate_heatmaps_from_population(data, kernel_area=1000000, resolution=30):
-    data = np.choose(data > 0, (0, data))
-    heatmap_km = get_heatmap(data, kernel_area, resolution)
+    #data = np.choose(data > 0, (0, data))
+    #heatmap_km = get_heatmap(data, kernel_area, resolution)
     data_points = np.choose(data > 0, (0, 1))
     heatmap_cover = get_heatmap(data_points, kernel_area, resolution)
-    return heatmap_km, heatmap_cover
+    print heatmap_cover.max(), heatmap_cover.min()
+    return heatmap_cover
 
 
-def run_for_file(addr, fout0='../madagascar/population_hrsl_km2.tiff', fout1='../madagascar/points_hrsl_km2.tiff'):
+def run_for_file(addr, fout1='../madagascar/points_hrsl_km2.tiff'):
     data = tiff_tools.read_array_from_tiff(addr)
-    data_km, data_points = generate_heatmaps_from_population(data)
-    tiff_tools.save_data_derived_from_tiff(addr, data_km, fout0, dtype=np.uint16, mincolor=0, cmap=tiff_tools.plt.cm.jet, logcolor=1)
-    tiff_tools.save_data_derived_from_tiff(addr, data_points, fout1, dtype=np.uint16, maxcolor=255, mincolor=0, cmap=tiff_tools.plt.cm.jet)
+    data = np.choose(data==127, (1, 0))
+    print data.sum(), data.min(), data.max()
+    data_points = generate_heatmaps_from_population(data, kernel_area = 1000*1000*np.pi)
+    tiff_tools.save_data_derived_from_tiff(addr, data_points, fout1, dtype=np.uint16, maxcolor=8000, mincolor=0, cmap=tiff_tools.plt.cm.jet)
+
+if __name__ == '__main__':
+    import sys
+    fin = sys.argv[1]
+    fout1 = sys.argv[2]
+    run_for_file(fin, fout1)
